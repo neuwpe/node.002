f5b28d17e75ec900d9935cdc97182e7e9a81ab63
tsuyoshi higuchi <a@wpe.jp>
2012-08-27 23:37:15 +0900

    test004

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
 		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.end('Hello Node Ninja & World 4 \n');
+		res.end('<div><input type="text" style="width:500px;" /></div><p>Hello Node Ninja & World 4</p> \n');
 }).listen(8080);
 
