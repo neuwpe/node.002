9e0ef7707732be3896e3d90bdafbd78135dd049a
tsuyoshi higuchi <a@wpe.jp>
2012-08-28 15:14:14 +0900

    s

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ var app = module.exports = express.createServer();
 app.get('/', function(req, res){
 	res.send('hello world 4');
 });
+app.set('title', 'My Site');
+app.get('title');
 app.listen(8080);
