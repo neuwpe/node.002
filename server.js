65e313f595066c0981463e41e852f5c574a56433
tsuyoshi higuchi <a@wpe.jp>
2012-08-28 14:44:02 +0900

    t

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var http = require('http');
 var app = module.exports = express.createServer();
 app.get('/', function(req, res){
-	res.send('hello world 2');
+	res.send('<h1>hello world 3</h1>');
 });
 app.listen(8080);
