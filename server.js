2a269648d34eff6906b733e9df6754ee49510a97
tsuyoshi higuchi <a@wpe.jp>
2012-08-28 16:27:54 +0900

    t

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = module.exports = express.createServer();
-app.set('title', 'わーわーわー');
+var io = require('socket.io').listen(app);
+
 app.get('/:id?', function(req, res, next){
 	var id = req.params.id;
 	res.send('hello world 6'+id? ": id="+id : "" );
