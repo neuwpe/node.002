f90dde1ca36e1d0ca1c78d4124fd213eb496e83f
tsuyoshi higuchi <a@wpe.jp>
2012-08-28 13:55:25 +0900

    ttt

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = module.exports = express.createServer();
 app.get('/', function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
+	//res.writeHead(200, {'Content-Type': 'text/html'});
 	res.send('hello world');
 	res.end('<h1>テスト</h1><div><input type="text" style="width:500px;" /></div><p>Hello Node Ninja & World 8</p> \n');
 });
